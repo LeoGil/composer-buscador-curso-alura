088c26cbd8e7ea68c590f624dd3dd69fdb2b74b6
Leonardo Gil <leogilc@gmail.com>
2024-11-28 15:17:39 -0300

    first commit

---
+++ buscar-cursos.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'vendor/autoload.php';
+// require 'src/Buscador.php';
+
+use GuzzleHttp\Client;
+use LeoGil\BuscadorDeCursoAlura\Buscador;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['verify' => false, 'base_uri' => 'https://www.alura.com.br/']);
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo $curso->textContent . PHP_EOL;
+}
